53805c93d5c5ee458f28db54f4a5bea7f412861e
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-04-04 05:09:39 -0300

    010

---
+++ Nivel-01/ex010-[Usando um JavaScript externo]/index.js
@@ -0,0 +1,14 @@
+let res = window.document.querySelector('#res')
+
+function botão1(){
+    res.innerHTML += '<br>Você escolheu >>> BOTÃO 1'
+}
+function botão2(){
+    res.innerHTML += '<br>Você escolheu >>> BOTÃO 2'
+}
+function botão3(){
+    res.innerHTML += '<br>Você escolheu >>> BOTÃO 3'
+}
+function botão4(){
+    res.innerHTML += '<br>Você escolheu >>> BOTÃO 4'
+}
